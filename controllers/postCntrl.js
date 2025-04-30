42420df29a85305d14fe6037381a5e2f4637d735
Anthony Arias <aarias92@gmail.com>
2025-04-30 18:09:23 -0500

    attempting to add addpost functionality

--- controllers/postCntrl.js
+++ controllers/postCntrl.js
@@ -8,12 +8,37 @@ export const getPosts = (req, res) => {
 
         return res.status(200).json(data);
     })
+        /*const q = "SELECT * FROM posts WHERE cat=?";
+
+        db.query(q, [req.params.cat], (err, data) => {
+            if (err) return res.send(err);
+    
+            return res.status(200).json(data);
+        })*/
+
 }
 export const getPost = (req, res) => {
     res.json("from controller")
 }
 export const addPost = (req, res) => {
-    res.json("from controller")
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("Not authenticated");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if(err) return res.status(403).json("Token is not valid");
+        const q = "INSERT INTO posts (`title`, `description`, `cat`) VALUES (?)"
+
+        const values = [
+            req.body.title,
+            req.body.description,
+            req.body.cat
+        ];
+
+        db.query(q, [values], (err, data) => {
+            if (err) return res.status(500).json(err)
+            return res.json("Post has been created");
+        })
+    });
 }
 export const deletePost = (req, res) => {
     res.json("from controller")
